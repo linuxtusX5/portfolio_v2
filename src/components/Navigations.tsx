f8b3c8a6359e288a3adf84f19fbaebe7858709b9
linuxtusX5 <salvatusjoffrey@gmail.com>
2024-05-16 17:01:29 +0800

    adding tooltip

--- src/components/Navigations.tsx
+++ src/components/Navigations.tsx
@@ -3,30 +3,32 @@ import Home from "../assets/home.png";
 import Skills from "../assets/skill.png";
 import Projects from "../assets/project.png";
 import Contact from "../assets/contacts.png";
+import { Button } from "flowbite-react";
+import { Tooltip } from "flowbite-react";
 
 function Navigations() {
   return (
     <div className="relative md:justify-between md:items-center md:flex md:h-full md:w-full lg:justify-center">
-      <div className="bg-MainBG rounded-2xl top-[20px] h-[75px] w-[12vh] flex justify-center items-center lg:absolute">
-        <img className="absolute h-[50px] w-[50px] " src={Home} alt="Home" />
-      </div>
-      <div className="bg-MainBG rounded-2xl top-[17vh] h-[75px] w-[12vh] flex justify-center items-center lg:absolute">
-        <img className="absolute h-[50px] w-[50px]" src={Skills} alt="Skills" />
-      </div>
-      <div className="bg-MainBG rounded-2xl top-[31vh] h-[75px] w-[12vh] flex justify-center items-center lg:absolute">
-        <img
-          className="absolute h-[50px] w-[50px]"
-          src={Projects}
-          alt="Projects"
-        />
-      </div>
-      <div className="bg-MainBG rounded-2xl top-[45vh] h-[75px] w-[12vh] flex justify-center items-center lg:absolute">
-        <img
-          className="absolute h-[50px] w-[50px]"
-          src={Contact}
-          alt="Contact"
-        />
-      </div>
+      <Button className="bg-MainBG relative rounded-2xl lg:top-[20px] h-[75px] w-[12vh] flex justify-center items-center lg:absolute">
+        <Tooltip content="Home" placement="left" style="light">
+          <img className="h-full w-full " src={Home} alt="Home" />
+        </Tooltip>
+      </Button>
+      <Button className="bg-MainBG rounded-2xl lg:top-[17vh] h-[75px] w-[12vh] flex justify-center items-center lg:absolute">
+        <Tooltip content="Skills" placement="left" style="light">
+          <img className="h-full w-full " src={Skills} alt="Skills" />
+        </Tooltip>
+      </Button>
+      <Button className="bg-MainBG rounded-2xl lg:top-[31vh] h-[75px] w-[12vh] flex justify-center items-center lg:absolute">
+        <Tooltip content="Projects" placement="left" style="light">
+          <img className="h-full w-full" src={Projects} alt="Projects" />
+        </Tooltip>
+      </Button>
+      <Button className="bg-MainBG rounded-2xl lg:top-[45vh] h-[75px] w-[12vh] flex justify-center items-center lg:absolute">
+        <Tooltip content="Contact" placement="left" style="light">
+          <img className="h-full w-full" src={Contact} alt="Contact" />
+        </Tooltip>
+      </Button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
+import flowbite from "flowbite-react/tailwind";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    // "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    flowbite.content(),
+  ],
   theme: {
     screens: {
       sm: "480px",
@@ -19,5 +24,8 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // require("flowbite/plugin")
+    flowbite.plugin(),
+  ],
 };
