4292a397b6028ffd110789b30c4b155132888941
linuxtusX5 <salvatusjoffrey@gmail.com>
2024-05-16 12:29:36 +0800

    navigation

--- src/components/Navigations.tsx
+++ src/components/Navigations.tsx
@@ -8,24 +8,24 @@ function Navigations() {
   return (
     <div className="relative md:justify-between md:items-center md:flex md:h-full md:w-full lg:justify-center">
       <div className="bg-MainBG rounded-2xl top-[20px] h-[75px] w-[12vh] flex justify-center items-center lg:absolute">
-        {/* <img className="absolute h-[50px] w-[50px] " src={Home} alt="Home" /> */}
+        <img className="absolute h-[50px] w-[50px] " src={Home} alt="Home" />
       </div>
       <div className="bg-MainBG rounded-2xl top-[17vh] h-[75px] w-[12vh] flex justify-center items-center lg:absolute">
-        {/* <img className="absolute h-[50px] w-[50px]" src={Skills} alt="Skills" /> */}
+        <img className="absolute h-[50px] w-[50px]" src={Skills} alt="Skills" />
       </div>
       <div className="bg-MainBG rounded-2xl top-[31vh] h-[75px] w-[12vh] flex justify-center items-center lg:absolute">
-        {/* <img
+        <img
           className="absolute h-[50px] w-[50px]"
           src={Projects}
           alt="Projects"
-        /> */}
+        />
       </div>
       <div className="bg-MainBG rounded-2xl top-[45vh] h-[75px] w-[12vh] flex justify-center items-center lg:absolute">
-        {/* <img
+        <img
           className="absolute h-[50px] w-[50px]"
           src={Contact}
           alt="Contact"
-        /> */}
+        />
       </div>
     </div>
   );
