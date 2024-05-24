36b47a8ffcb9230f19456790cc0ef95736561115
linuxtusX5 <salvatusjoffrey@gmail.com>
2024-05-24 20:22:20 +0800

    adding social media link

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,41 +1,47 @@
-import React from "react";
 import facebook from "../assets/facebook.png";
 import github from "../assets/code.png";
 import linkedin from "../assets/linkedin.png";
+import { Button } from "flowbite-react";
 
 function SocialMedia() {
   return (
-    <div className="relative lg:h-[233px] justify-center items-center flex h-[233px]">
-      <div className="absolute bg-MainBG rounded-2xl top-[20px] h-[55px] w-[40vh] flex justify-center items-center">
-        <img
-          className="absolute h-[40px] w-[40px] left-[20px]"
-          src={facebook}
-          alt="facebook"
-        />
-        <strong className="absolute text-[#ffffff] text-[3vh] left-[13vh]">
-          Salvatus Dev
-        </strong>
-      </div>
-      <div className="absolute bg-MainBG rounded-2xl top-[13vh] h-[55px] w-[40vh] flex justify-center items-center">
-        <img
-          className="absolute h-[40px] w-[40px] left-[20px]"
-          src={github}
-          alt="github"
-        />
-        <strong className="absolute text-[#ffffff] text-[3vh] left-[13vh]">
-          linuxtusX5
-        </strong>
-      </div>
-      <div className="absolute bg-MainBG rounded-2xl top-[23vh] h-[55px] w-[40vh] flex justify-center items-center">
-        <img
-          className="absolute h-[40px] w-[40px] left-[20px]"
-          src={linkedin}
-          alt="linkedin"
-        />
-        <strong className="absolute text-[#ffffff] text-[3vh] left-[13vh]">
-          joffrey salvatus
-        </strong>
-      </div>
+    <div className="relative lg:h-[233px] justify-center items-center flex ">
+      <Button className="absolute bg-MainBG rounded-2xl top-[20px] h-[55px] w-[40vh]">
+        <div className="flex justify-center items-center relative w-[40vh]">
+          <img
+            className="h-[40px] w-[40px] absolute left-[5vh]"
+            src={facebook}
+            alt="facebook"
+          />
+          <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+            Salvatus Dev
+          </strong>
+        </div>
+      </Button>
+      <Button className="absolute bg-MainBG rounded-2xl top-[13vh] h-[55px] w-[40vh]">
+        <div className="flex justify-center items-center relative w-[40vh]">
+          <img
+            className="h-[40px] w-[40px] absolute left-[5vh]"
+            src={github}
+            alt="github"
+          />
+          <strong className="text-[#ffffff] text-[3vh] left-[13vh] absolute">
+            linuxtusX5
+          </strong>
+        </div>
+      </Button>
+      <Button className="absolute bg-MainBG rounded-2xl top-[23vh] h-[55px] w-[40vh]">
+        <div className="flex justify-center items-center relative w-[40vh]">
+          <img
+            className="h-[40px] w-[40px] absolute left-[5vh]"
+            src={linkedin}
+            alt="linkedin"
+          />
+          <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+            joffrey salvatus
+          </strong>
+        </div>
+      </Button>
     </div>
   );
 }
