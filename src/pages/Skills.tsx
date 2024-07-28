9820a25cbc3cb62514d6f56f534fba298e3cccbc
linuxtusX5 <salvatusjoffrey@gmail.com>
2024-07-28 14:38:12 +0800

    adding skills

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,10 +1,208 @@
 import React from "react";
+import HTML from "../assets/html.png";
+import CSS from "../assets/css-3.png";
+import JS from "../assets/js.png";
+import REACT from "../assets/react.png";
+import MONGO from "../assets/database.png";
+import NODEJS from "../assets/nodejs.png";
+import EXPRESS from "../assets/expressjs.png";
+import TYPESCRIPT from "../assets/typescript.png";
+import TAILWINDCSS from "../assets/tailwindcss.png";
+import BOOTSTRAP from "../assets/bootstrap.png";
+import C from "../assets/c++.png";
+import JAVA from "../assets/java.png";
+import MYSQL from "../assets/mysql.png";
+import database from "../assets/database.png";
+import FIGMA from "../assets/figma.png";
+import GITHUB from "../assets/github.png";
+import GIT from "../assets/git.png";
 
 function Skills() {
   return (
     <div className="relative flex justify-center items-center">
-      <div className="bg-MainBG rounded-2xl absolute h-[93vh] w-full top-5 flex justify-center items-center">
-        Skills
+      <div className="overflow-y-auto  flex justify-center bg-MainBG rounded-2xl absolute h-[93vh] w-full top-5 p-[10px]">
+        <div className="h-[90vh] w-[40vh] top-5 lg:items-center lg:w-full lg:grid lg:grid-cols-3">
+          <div className="relative bg-second rounded-2xl ] h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={HTML}
+              alt="HTML"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              HTML
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={CSS}
+              alt="CSS"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              CSS
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={JS}
+              alt="JAVASCRIPT"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              JAVASCRIPT
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={REACT}
+              alt="REACTJS"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              REACTJS
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={MONGO}
+              alt="MONGODB"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              MONGODB
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={NODEJS}
+              alt="NODEJS"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              NODEJS
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={REACT}
+              alt="REACTNATIVE"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              REAC NATIVE
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={EXPRESS}
+              alt="EXPRESSJS"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              EXPRESS JS
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={TYPESCRIPT}
+              alt="TYPESCRIPT"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              TYPESCRIPT
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={TAILWINDCSS}
+              alt="TAILWINDCSS"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              TAILWIND CSS
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={BOOTSTRAP}
+              alt="BOOTSTRAP"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              BOOTSTRAP
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={C}
+              alt="C++"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              C++
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={JAVA}
+              alt="JAVA"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              JAVA
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={MYSQL}
+              alt="MYSQL"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              MYSQL
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={database}
+              alt="FIREBASE"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              FIREBASE
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={FIGMA}
+              alt="FIGMA"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              FIGMA
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={GITHUB}
+              alt="GITHUB"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              GITHUB
+            </strong>
+          </div>
+          <div className="relative bg-second rounded-2xl h-[55px] w-[40vh] flex justify-center items-center mb-2">
+            <img
+              className="h-[40px] w-[40px] absolute left-[5vh]"
+              src={GIT}
+              alt="GIT"
+            />
+            <strong className=" text-[#ffffff] text-[3vh] left-[13vh] absolute">
+              GIT
+            </strong>
+          </div>
+        </div>
       </div>
     </div>
   );
