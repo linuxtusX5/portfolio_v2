457fb0452dca9322ce238edbbef9aef65521e8d2
linuxtusX5 <salvatusjoffrey@gmail.com>
2024-08-13 12:29:10 +0800

    adding contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,82 @@
-import React from "react";
+import { Button } from "flowbite-react";
+import React, { useRef, FormEvent } from "react";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Contact() {
+  const form = useRef<HTMLFormElement | null>(null);
+
+  const sendEmail = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm(
+          "service_t9564z3",
+          "template_gcp44gu",
+          form.current,
+          "user_zGZ7M9G7Yd20lIrSMAwL9"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            toast.success("Email sent successfully!", {
+              position: "bottom-left",
+              className: "foo-bar",
+            });
+          },
+          (error) => {
+            console.log(error.text);
+            toast.error("Failed to send email. Please try again.", {
+              position: "bottom-left",
+              className: "foo-bar",
+            });
+          }
+        );
+    }
+  };
   return (
     <div className="relative flex justify-center items-center">
-      <div className="bg-MainBG rounded-2xl absolute h-[93vh] w-full top-5 flex justify-center items-center">
-        Contact
+      <div className="bg-MainBG rounded-2xl absolute h-[93vh] w-full top-5 flex justify-center ">
+        <ToastContainer />
+        <div className="text-center mt-8">
+          <h3 className="text-4xl font-semibold text-white">
+            Contact <span className="text-second">Me</span>
+          </h3>
+          <p className="text-gray-400 mt-3 text-lg">Get in touch</p>
+
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            className="flex flex-col flex-1 gap-3 w-[45vh] lg:w-[60vh] pt-3"
+          >
+            <input
+              className="rounded-2xl"
+              type="text"
+              name="Name"
+              placeholder="Your Name"
+            />
+            <input
+              className="rounded-2xl"
+              type="Email"
+              name="Email"
+              placeholder="Your Email Address"
+            />
+            <textarea
+              className="rounded-2xl"
+              placeholder="Your Message"
+              name="message"
+              rows={10}
+            ></textarea>
+            <Button
+              type="submit"
+              className="bg-second btn-primary flex items-center justify-center"
+            >
+              Send Message
+            </Button>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,15 +2,26 @@ import React from "react";
 import { Card } from "flowbite-react";
 import p1 from "../assets/p1.png";
 import p2 from "../assets/p2.png";
+import p3 from "../assets/p3.png";
+import p4 from "../assets/p4.png";
+import p5 from "../assets/p5.jpg";
 
 function Projects() {
   const PROJECT1 = process.env.REACT_APP_PROJECT1_PAGE_URL;
+  const PROJECT2 = process.env.REACT_APP_PROJECT2_PAGE_URL;
+  const PROJECT3 = process.env.REACT_APP_PROJECT3_PAGE_URL;
+  const PROJECT4 = process.env.REACT_APP_PROJECT4_PAGE_URL;
+  const REPOSITORY5 = process.env.REACT_APP_PROJECT5_PAGE_URL;
+
   const REPOSITORY1 = process.env.REACT_APP_P1_REPOSITORY_PAGE_URL;
+  const REPOSITORY2 = process.env.REACT_APP_P2_REPOSITORY_PAGE_URL;
+  const REPOSITORY3 = process.env.REACT_APP_P3_REPOSITORY_PAGE_URL;
+  const REPOSITORY4 = process.env.REACT_APP_P4_REPOSITORY_PAGE_URL;
 
   return (
     <div className="relative flex justify-center items-center h-screen">
       <div className="overflow-y-auto bg-MainBG rounded-2xl absolute h-[93vh] w-full flex justify-center ">
-        <div className="h-min w-full absolute bg-debug p-[20px] flex flex-col gap-5">
+        <div className=" w-full absolute flex flex-col gap-5 h-[100%] width-[100%] m-[0px] p-[20px] overflow-x-hidden">
           <Card
             className=" w-full lg:w-auto bg-second mb-[20px] p-[10px] lg:p-[20px] border-none"
             imgAlt="Appointment"
@@ -58,7 +69,7 @@ function Projects() {
             <h5 className="flex justify-start text-2xl font-bold tracking-tight text-white">
               Student Council
             </h5>
-            <div className="grid lg:flex gap-3 w-[80vh] ml-[-20px]">
+            <div className="grid lg:flex gap-3 w-[80vh] ml-[-10px] lg:ml-0">
               <div className="w-[40vh]">
                 <h3 className="flex justify-start font-bold  text-white">
                   Student:
@@ -87,7 +98,7 @@ function Projects() {
             </div>
             <div className="flex gap-x-2">
               <a
-                href={PROJECT1}
+                href={PROJECT2}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex justify-center items-center   rounded-2xl h-[40px] w-[80px] bg-MainBG"
@@ -98,7 +109,7 @@ function Projects() {
               </a>
 
               <a
-                href={REPOSITORY1}
+                href={REPOSITORY2}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex justify-center items-center   rounded-2xl h-[40px] w-[80px] bg-MainBG"
@@ -110,39 +121,99 @@ function Projects() {
             </div>
           </Card>
 
-          {/* Additional cards here */}
           <Card
-            className="w-full lg:w-auto bg-second p-[10px] lg:p-[20px] border-none"
-            imgAlt="Appointment"
-            imgSrc={p1}
+            className=" w-full lg:w-auto bg-second mb-[20px] p-[10px] lg:p-[20px] border-none"
+            imgAlt=" AdvanceCare"
+            imgSrc={p3}
           >
-            <h5 className="text-2xl font-bold tracking-tight text-white">
-              Medical Appointment
+            <h5 className="flex justify-start text-2xl font-bold tracking-tight text-white">
+              Advance Care
             </h5>
-            <p className="font-normal text-white">Email: yoon@gmail.com</p>
-            <p className="font-normal text-white">Password: 123</p>
             <div className="flex gap-x-2">
               <a
-                href={PROJECT1}
+                href={PROJECT3}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex justify-center items-center rounded-2xl h-[40px] w-[80px] bg-MainBG"
+                className="flex justify-center items-center   rounded-2xl h-[40px] w-[80px] bg-MainBG"
               >
-                <p className="text-sm text-white">Live Demo</p>
+                <p className="flex justify-start text-sm text-white">
+                  Live Demo
+                </p>
               </a>
 
               <a
-                href={REPOSITORY1}
+                href={REPOSITORY3}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex justify-center items-center rounded-2xl h-[40px] w-[80px] bg-MainBG"
+                className="flex justify-center items-center   rounded-2xl h-[40px] w-[80px] bg-MainBG"
               >
-                <p className="text-sm text-white">Repository</p>
+                <p className="flex justify-start text-sm text-white">
+                  Repository
+                </p>
+              </a>
+            </div>
+          </Card>
+          <Card
+            className=" w-full lg:w-auto bg-second mb-[20px] p-[10px] lg:p-[20px] border-none"
+            imgAlt="Todo"
+            imgSrc={p4}
+          >
+            <h5 className="flex justify-start text-2xl font-bold tracking-tight text-white">
+              Todo App
+            </h5>
+            <p className="flex justify-start font-normal  text-white">
+              Email: yoon@gmail.com
+            </p>
+            <p className="flex justify-start font-normal  text-white">
+              Password: 123
+            </p>
+            <div className="flex gap-x-2">
+              <a
+                href={PROJECT4}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex justify-center items-center   rounded-2xl h-[40px] w-[80px] bg-MainBG"
+              >
+                <p className="flex justify-start text-sm text-white">
+                  Live Demo
+                </p>
+              </a>
+
+              <a
+                href={REPOSITORY4}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex justify-center items-center   rounded-2xl h-[40px] w-[80px] bg-MainBG"
+              >
+                <p className="flex justify-start text-sm text-white">
+                  Repository
+                </p>
               </a>
             </div>
           </Card>
 
-          {/* Repeat for more cards */}
+          <Card
+            className=" w-[50vh] lg:w-auto bg-second mb-[20px] p-[10px] lg:p-[20px] border-none flex justify-center items-center"
+            imgAlt="TaskManagement"
+          >
+            {" "}
+            <img alt="Bonnie" src={p5} className="h-[100%] w-[80vh]" />
+            <h5 className="flex justify-start text-2xl font-bold tracking-tight text-white">
+              Task Management
+            </h5>
+            <div className="flex gap-x-2">
+              <a
+                href={REPOSITORY5}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex justify-center items-center   rounded-2xl h-[40px] w-[80px] bg-MainBG"
+              >
+                <p className="flex justify-start text-sm text-white">
+                  Download
+                </p>
+              </a>
+            </div>
+          </Card>
         </div>
       </div>
     </div>
